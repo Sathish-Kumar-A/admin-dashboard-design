558fa3c76ceecbd5ff7ebac1d50c4befc472cd14
Sathish Kumar A <sathish252199@gmail.com>
2022-04-01 08:29:39 +0530

    created template for admindashboard

---
+++ src/components/Sidebar/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaSmileWink } from "react-icons/fa";
+import "./sidebar.css";
+
+const SideBar = () => {
+  return (
+      <div className="w-25 wrapper">
+          <FaSmileWink className=''/>
+    </div>
+  )
+}
+
+export default SideBar
